c28923ab22298efb87a4bedec001fa87019eb108
Mohamed Mihammi <momihamm@e2r10p12.1337.ma>
2024-01-05 16:30:26 +0100

    feat :: starting a new day whit a new cpp challenge

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 10:24:51 by momihamm          #+#    #+#             */
+/*   Updated: 2024/01/05 12:32:24 by momihamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+class Zombie
+{
+    std::string name;
+	public :
+	void	announce(void);
+};
+
+#endif

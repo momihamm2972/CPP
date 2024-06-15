0143b47fc73a90d7c7fc32eea7ad05fcf63f942a
momihamm2972 <mmim22138@gmail.com>
2024-06-15 01:15:15 +0100

    cc cv les enfans cv

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,25 @@
 /*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 08:40:47 by momihamm          #+#    #+#             */
-/*   Updated: 2024/03/16 08:40:48 by momihamm         ###   ########.fr       */
+/*   Updated: 2024/03/31 16:46:29 by momihamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat
+{
+    private :
+        const std::string name;
+        int               grade;
+    public :
+        Bureaucrat();
+        std::string getName();
+        int         getGrade();
+        void    incrementBureaucrat(int &grade);
+        void    decrementBureaucrat(int &grade);
+};
 

7570d59e99599ef49efee237737404bdd1d8f076
Mohamed Mihammi <momihamm@e3r10p4.1337.ma>
2024-10-14 22:38:37 +0100

    ex00 is done

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/16 08:40:44 by momihamm          #+#    #+#             */
-/*   Updated: 2024/10/14 22:34:46 by momihamm         ###   ########.fr       */
+/*   Created: 2024/10/14 22:36:31 by momihamm          #+#    #+#             */
+/*   Updated: 2024/10/14 22:36:32 by momihamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
-
-int main()
-{
-    Bureaucrat jondi("bozbal", 3);
-    try
-    {
-        jondi.incrementGrade();
-    }
-    catch (std::exception & e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    // std::cout << jondi << std::endl;
-}

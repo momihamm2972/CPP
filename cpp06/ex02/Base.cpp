165041fc9800ae1e9a9858fe51ff62ff4fec38e1
Mohamed Mihammi <momihamm@e3r10p17.1337.ma>
2024-10-26 01:15:13 +0100

    kmi

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 23:41:03 by momihamm          #+#    #+#             */
-/*   Updated: 2024/10/26 01:05:51 by momihamm         ###   ########.fr       */
+/*   Updated: 2024/10/26 01:14:54 by momihamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void identify(Base* p)
 		std::cout << "A" << std::endl;
 	else if (dynamic_cast<B*>(p))
 		std::cout << "B" << std::endl;
-	else if (<C*dynamic_cast>(p))
+	else if (dynamic_cast<C*>(p))
 		std::cout << "C" << std::endl;
 	else
 		std::cout << "Unknown" << std::endl;
